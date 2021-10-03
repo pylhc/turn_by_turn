9256efbd5e51f0f34abab77c62ee73b275f25fcc
Felix Soubelet <felix.soubelet@protonmail.com>
2021-10-03 14:40:53 +0200

    don't write a stats.txt by default

--- turn_by_turn/readers/trackone.py
+++ turn_by_turn/readers/trackone.py
@@ -4,6 +4,7 @@ Trackone TbT Data Handler
 
 Data handling for turn-by-turn measurement files from the ``MAD-X`` code.
 """
+import logging
 from pathlib import Path
 from typing import Tuple, Union
 
@@ -11,6 +12,8 @@ import numpy as np
 
 from turn_by_turn.structures import TbtData
 from turn_by_turn.utils import numpy_to_tbts
+
+LOGGER = logging.getLogger()
 
 
 def read_tbt(file_path: Union[str, Path]) -> TbtData:
@@ -29,13 +32,14 @@ def read_tbt(file_path: Union[str, Path]) -> TbtData:
     return numpy_to_tbts(names, matrix[[0, 2]])
 
 
-def get_trackone_stats(file_path: Union[str, Path]) -> Tuple[int, int]:
+def get_trackone_stats(file_path: Union[str, Path], write_out: bool = False) -> Tuple[int, int]:
     """
     Determines the number of particles and turns in the matrices from the provided ``MAD-X``
     **trackone** file.
 
     Args:
         file_path (Union[str, Path]): path to the turn-by-turn measurement file.
+        write_out (bool): if ``True``, write out the determined stats to a **stats.txt** file.
 
     Returns:
         A tuple with the number of turns and particles.
@@ -60,8 +64,12 @@ def get_trackone_stats(file_path: Union[str, Path]) -> Tuple[int, int]:
             if parts[0] == "-1":
                 nparticles = 1
             stats_string = stats_string + line
-    with open("stats.txt", "w") as stats_file:
-        stats_file.write(stats_string)
+
+    if write_out:
+        LOGGER.debug(f"Writing tbt stats for file '{file_path.absolute()}' at 'stats.txt'")
+        with Path("stats.txt").open("w") as stats_file:
+            stats_file.write(stats_string)
+
     return nturns - 1, nparticles
 
 
